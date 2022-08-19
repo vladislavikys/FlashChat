b7cd188cab672660ea1e1f553145cdebd216a252
vlad <>
2022-08-19 17:43:20 +0300

    add chat

--- Flash Chat iOS13/Constans.swift
+++ Flash Chat iOS13/Constans.swift
@@ -6,7 +6,28 @@
 //  Copyright © 2022 Angela Yu. All rights reserved.
 //
 
-struct Constans {
+//struct Constans {
+//    static let registerSegue = "RegisterToChat"
+//    static let logunSegue = "LoginToChat"
+//}
+struct K {
+    static let appName = "⚡️FlashChat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
     static let registerSegue = "RegisterToChat"
-    static let logunSegue = "LoginToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
 }
--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -23,7 +23,7 @@ class LoginViewController: UIViewController {
                 if let e = error {
                     print(e.localizedDescription)
                 } else {
-                    self.performSegue(withIdentifier: Constans.logunSegue, sender: self)
+                    self.performSegue(withIdentifier: K.loginSegue, sender: self)
                 }
             }
         }
